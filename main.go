63a568ee3b76ce8e140279aba8fc76a858393899
Vinicius <vinicius@pop-os.localdomain>
2024-02-02 07:34:41 -0300

    finish game rules

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
+
+var score = 0
 
 func main() {
 	r := gin.Default()
@@ -21,6 +24,10 @@ func main() {
 		})
 	})
 
+	r.GET("/score", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, strconv.FormatInt(int64(score), 10))
+	})
+
 	r.GET("/choose/:choice", func(ctx *gin.Context) {
 		choice := ctx.Param("choice")
 
@@ -31,9 +38,86 @@ func main() {
 	})
 
 	r.GET("/result", func(ctx *gin.Context) {
+		userChoice := ctx.Query("user")
 		choices := [3]string{"rock", "paper", "scissors"}
 		random := rand.Intn(len(choices))
-		log.Println("Rando choice", choices[random])
+
+		for choices[random] == userChoice {
+			log.Println("Draw")
+			random = rand.Intn(len(choices))
+		}
+
+		if choices[random] == "rock" {
+			if userChoice == "paper" {
+				log.Println("User win!")
+				ctx.HTML(http.StatusOK, "win.tmpl", gin.H{
+					"choice": userChoice,
+					"house":  choices[random],
+				})
+				score = score + 1
+				return
+			} else {
+				log.Println("User lose!")
+				ctx.HTML(http.StatusOK, "lose.tmpl", gin.H{
+					"choice": userChoice,
+					"house":  choices[random],
+				})
+
+				if score > 0 {
+					score = score - 1
+				}
+				return
+			}
+		}
+
+		if choices[random] == "paper" {
+			if userChoice == "scissors" {
+				log.Println("User win!")
+				ctx.HTML(http.StatusOK, "win.tmpl", gin.H{
+					"choice": userChoice,
+					"house":  choices[random],
+				})
+				score = score + 1
+				return
+			} else {
+				log.Println("User lose!")
+				ctx.HTML(http.StatusOK, "lose.tmpl", gin.H{
+					"choice": userChoice,
+					"house":  choices[random],
+				})
+				if score > 0 {
+					score = score - 1
+				}
+				return
+			}
+		}
+
+		if choices[random] == "scissors" {
+			if userChoice == "rock" {
+				log.Println("User win!")
+				ctx.HTML(http.StatusOK, "win.tmpl", gin.H{
+					"choice": userChoice,
+					"house":  choices[random],
+				})
+				score = score + 1
+				return
+			} else {
+				log.Println("User lose!")
+				ctx.HTML(http.StatusOK, "lose.tmpl", gin.H{
+					"choice": userChoice,
+					"house":  choices[random],
+				})
+				if score > 0 {
+					score = score - 1
+				}
+				return
+			}
+		}
+
+	})
+
+	r.GET("/main", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "main.tmpl", gin.H{})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
