a58349ab6966612bbc6a82a2a94a81fb099f830e
Vinicius <vinicius@pop-os.localdomain>
2024-02-19 07:25:57 -0300

    feat add deploy script

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+echo "Killing current process"
+$PID=$(sudo lsof -i | grep rock | awk '{print $2}')
+
+# Check if the PID is not empty and greater than 0
+if [[ -n $pid && $pid -gt 0 ]]; then
+    echo "Stopping process with PID: $pid"
+    # Use kill command to stop the process
+    sudo kill $pid
+
+    # Check if the kill command was successful
+    if [ $? -eq 0 ]; then
+        echo "Process stopped successfully."
+    else
+        echo "Failed to stop the process."
+    fi
+else
+    echo "Unable to determine a valid process ID."
+fi
+
+go build -o rock-paper-scissors
+
+# Check if the kill command was successful
+if [ $? -eq 0 ]; then
+  echo "Build went successfully."
+  sudo nohup ./rock-paper-scissors &
+  exit 0
+else
+  echo "Build failed."
+  exit 1
+fi
+
+
+
